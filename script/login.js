4296a04d2ae9def3abdc3875a2993a8b84d9988c
Mishu78 <meherun2002@gmail.com>
2026-02-18 15:05:41 +0600

    getting mobile number,pin number and matching them

--- script/login.js
+++ script/login.js
@@ -1,1 +1,22 @@
 console.log("login functionality");
+
+//login button work
+document.getElementById("login-btn").addEventListener("click",function(){
+    //1.get the mobile number
+    //2.get the pin
+    //3.match pin and mobile number
+    //3-1.true:::>>alert>homepage
+    //3-2.false:::>alert>return
+    //console.log("login button clicked");
+    const inputNumber=document.getElementById("input-number");
+    const contactNumber=inputNumber.value;
+    console.log(contactNumber);
+
+    const inputPin=document.getElementById("input-pin");
+    const contactPin=inputPin.value;
+    console.log(contactPin);
+
+    if(contactNumber == "01234567890" && contactPin == "1234"){
+  alert("login Successful");
+    }
+})
