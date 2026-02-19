6abe429b92cb59f6574cd8940893d3a5a3900577
Mishu78 <meherun2002@gmail.com>
2026-02-19 09:59:59 +0600

    get the agent number and amount to withdraw

--- script/cashout.js
+++ script/cashout.js
@@ -6,4 +6,15 @@ document.getElementById("cashout-btn").addEventListener("click",function(){
     //5.get the pin and verify
     //5-1.true:show an alert>set balance
     //5-2-false:show an error alert >return
+
+    //1.
+    const cashoutNumberInput=document.getElementById('cashout-number');
+    const cashoutNumber=cashoutNumberInput.value;
+    console.log(cashoutNumber);
+
+    //2.
+    const cashoutAmountInput=document.getElementById("cashout-amount");
+    const cashoutAmount=cashoutAmountInput.value;
+    console.log(cashoutAmount);
 })
+
